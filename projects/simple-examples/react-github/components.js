e9fc63524cde0ad71c61a047ed1dc07c089bfcca
Jamie Countryman <jamie.countryman@gmail.com>
2016-01-26 19:44:15 -0600

    Get updated code

--- projects/simple-examples/react-github/components.js
+++ projects/simple-examples/react-github/components.js
@@ -120,7 +120,6 @@ var CommitsComponent = React.createClass({
 
         if (skipTimeouts) {
             $.get('https://api.github.com/repos/' + this.state.repo + '/commits?sha=' + branch, (data, textStatus, request) => {
-                console.log('success!');
                 this.setState({ 'commits': data, 'apiRemaining': request.getResponseHeader('X-RateLimit-Remaining'), 'apiReset': parseInt(request.getResponseHeader('X-RateLimit-Reset')) * 1000 });
             }).fail(response => this.setState({ 'apiRemaining': request.getResponseHeader('X-RateLimit-Remaining'), 'apiReset': parseInt(request.getResponseHeader('X-RateLimit-Reset')) * 1000 }));
         } else {
@@ -150,7 +149,6 @@ var CommitsComponent = React.createClass({
     },
 
     branchChange: function (branch) {
-        console.log('branch changed to ' + branch);
         this.stopTimeouts();
         this.setState({ 'branch': branch });
         this.getCommits(true, branch);
